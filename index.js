d9123b6a6e3f80fec6bf45effdfa571ee0bd065b
dev2 <c.chaiwat.ch@gmail.com>
2021-10-12 18:53:24 +0700

    update: port for deploy

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
   res.send('<h1>Welcome to Express</h1>')
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening to port 3000')
 })
